9d3c3ea75bfcdb329610e51dcffa82d7f6aeac8f
NguyenHaiVuong <vuongnguyen20012001@gmail.com>
2022-09-25 11:16:44 +0700

    first commit

---
+++ lib/screens/allHotel_Screen.dart
@@ -0,0 +1,171 @@
+
+import 'package:app_booking/screens/addHotel_Screen.dart';
+import 'package:app_booking/screens/destination_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'editHotel_Screen.dart';
+
+late User loggedInUser;
+class allHotel_Screen extends StatefulWidget {
+  allHotel_Screen({ required this.documentSnapshot});
+  DocumentSnapshot? documentSnapshot;
+  @override
+  State<allHotel_Screen> createState() => _allHotel_ScreenState();
+}
+
+class _allHotel_ScreenState extends State<allHotel_Screen> {
+  final String admin = "admin@email.com";
+  final _auth = FirebaseAuth.instance;
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+  void getCurrentUser() async {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print('User: $user');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+  final CollectionReference _allHotel = FirebaseFirestore.instance.collection('allHotel');
+  Future<void> deleteHotel(String hotelID) async {
+    await _allHotel.doc(hotelID).delete();
+    EasyLoading.showSuccess('Xóa thành công',
+      duration: Duration(milliseconds: 1300),
+      maskType: EasyLoadingMaskType.black,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    var currentuser = loggedInUser.email;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Center(child: Text('Khách sạn tại ' + widget.documentSnapshot!['nameCity'], style: TextStyle(
+                color: Colors.blueGrey.shade800,
+                fontFamily: 'Vollkorn',
+              fontSize: 20,
+            ),)),
+            currentuser == admin ?
+            GestureDetector(
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) =>
+                          addHotel_Screen(documentSnapshot: widget.documentSnapshot,)));
+                },
+                child: Icon(FontAwesomeIcons.circlePlus,
+                  color: Colors.blueGrey.shade800,
+                  size: 30,)): SizedBox(),
+          ],
+        ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('allCity').
+        doc(widget.documentSnapshot!.id).collection('allHotel').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(!snapshot.hasData) return Text('Erorr');
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index){
+                DocumentSnapshot document = snapshot.data!.docs[index];
+                return Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: GestureDetector(
+                    onTap: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) =>
+                            DestinationScreen(documentSnapshot: document))),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topRight,
+                          width: width,
+                          height: width*0.5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topRight: Radius.circular(15), topLeft: Radius.circular(15)),
+                            image: DecorationImage(
+                              image: NetworkImage(document['imageUrl']),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: width,
+                              height: height * 0.05,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  currentuser == admin ?
+                                  GestureDetector(
+                                    onTap: () => Navigator.push(context,
+                                      MaterialPageRoute(builder:(context) =>
+                                          editHotel_Screen(document),),),
+                                    child: Container(
+                                        height: width *0.12,
+                                        width: width *0.12,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey.shade600,
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        child: Icon(FontAwesomeIcons.edit, color: Colors.white,)),
+                                  ): SizedBox(),
+                                  SizedBox(width: 5,),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: width,
+                          height: width*0.2,
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey.shade200,
+                            borderRadius: BorderRadius.only(bottomRight: Radius.circular(15), bottomLeft: Radius.circular(15)),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(document['nameHotel'], style: TextStyle(
+                                  fontSize: 27,
+                                  fontFamily: 'Vollkorn',
+                                ),),
+                                Text('VND ' + document['price'],
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/all_City.dart
@@ -0,0 +1,170 @@
+import 'package:app_booking/screens/addCityAll.dart';
+import 'package:app_booking/screens/editCity.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import '../screens/destination_screen.dart';
+import 'allHotel_Screen.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+class all_City extends StatefulWidget {
+  const all_City({Key? key}) : super(key: key);
+
+  @override
+  State<all_City> createState() => _all_CityState();
+}
+
+class _all_CityState extends State<all_City> {
+  final _auth = FirebaseAuth.instance;
+  late User loggedInUser;
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print('e');
+    }
+  }
+  final CollectionReference _allCity = FirebaseFirestore.instance.collection('allCity');
+  String admin = "admin@email.com";
+  Future<void> delete(String cityID) async {
+    await _allCity.doc(cityID).delete();
+    EasyLoading.showSuccess('Xóa thành công!',
+      duration: Duration(milliseconds: 1300),
+      maskType: EasyLoadingMaskType.black,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade300,
+        automaticallyImplyLeading: false,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Center(child: Text('TẤT CẢ ĐỊA ĐIỂM', style: TextStyle(
+              color: Colors.blueGrey.shade800,
+              fontFamily: 'Vollkorn'
+            ),)),
+            loggedInUser.email == admin ?
+            GestureDetector(
+              onTap: () => Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => addCity_Screen())),
+                child: Icon(FontAwesomeIcons.circlePlus,
+                  color: Colors.grey,
+                  size: 35,)) : SizedBox(),
+          ],
+        ),
+      ),
+      body: StreamBuilder(
+        stream: _allCity.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(!snapshot.hasData)
+            return SpinKitSquareCircle(
+            color: Colors.white,
+            size: 50.0,
+             duration: Duration(milliseconds: 1400),
+          );
+          return GridView.builder(
+            padding: EdgeInsets.all(15),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: width * 0.5,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1,
+            ),
+            itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, int index){
+              DocumentSnapshot documentSnapshot = snapshot.data!.docs[index];
+              return Stack(
+                alignment: Alignment.bottomLeft,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) =>
+                                allHotel_Screen(documentSnapshot: documentSnapshot,)));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            image: DecorationImage(
+                              image: NetworkImage(documentSnapshot['imageUrl']),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+                            ),
+                        ),
+                      ),
+                    ),
+                    loggedInUser.email == admin ?
+                    Positioned(
+                      top: 5, right: 50,
+                      child: GestureDetector(
+                        onTap: () => Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => editCity_Screen(documentSnapshot))),
+                        child: Container(
+                          width: width * 0.1,
+                          height: width * 0.1,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade400,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(FontAwesomeIcons.edit),
+                        ),
+                      ),): SizedBox(),
+                    loggedInUser.email == admin ?
+                    Positioned(
+                      top: 5, right: 5,
+                      child: GestureDetector(
+                        onTap: (){
+                          delete(documentSnapshot.id);
+                        },
+                        child: Container(
+                          width: width * 0.1,
+                          height: width * 0.1,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade400,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(FontAwesomeIcons.trashCan),
+                        ),
+                      ),): SizedBox(),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 5,),
+                          Text(documentSnapshot['nameCity'],
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.white,
+                              fontFamily: 'Vollkorn',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+              );
+            },
+          );
+        },
+      )
+    );
+  }
+}
